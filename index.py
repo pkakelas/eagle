8316e1f02e67cd2c94f002c631cfcd70d490cd13
Dimitris Lamprinos <pkakelas@gmail.com>
2019-06-23 23:57:03 +0300

    Remove send email functionality

--- index.py
+++ index.py
@@ -12,22 +12,6 @@ else:
 
 print(time.strftime("%x") + ": Eagle woke up")
 
-def sendMail(amount):
-    """
-    r = request.post(
-        "https://api.mailgun.net/v3/sandboxb0290be0c6374214b94158e8986a61b2.mailgun.org/messages",
-        auth=("api", "key-7f1ccf639b43e107cda773f9eec84ea5"),
-        data={"from": "Eagle <postmaster@sandboxb0290be0c6374214b94158e8986a61b2.mailgun.org>",
-              "to": config['myName'] + " <" + config['myMail'] + ">",
-              "subject": "Crypto finance report",
-              "text": "Good evening Mitsara,\r\n\r\nIf you sell everything you have right now with the bid price you would have in total " + str(amount) + "BTC. \r\nThis equals to " + str(amount * lastPrice) + " euro. \r\n\r\nHave a beautiful day,\r\nEagle"})
-
-    if (r.status_code) == 200:
-        return True
-    else:
-        return False
-    """
-
 total_volume = 0
 symbols = ','.join(config['currencies'])
 url = "http://api.coinlayer.com/api/live?access_key=" + config['coinlayer'] + "&target=EUR&symbols=" + symbols
@@ -43,8 +27,3 @@ with request.urlopen(url) as response:
        total_volume += rates[currency] * config['currencies'][currency]['balance']
 
 print("Total euro : " + str(total_volume) + " eur")
-
-if (sendMail(total_volume)):
-    print("Email sent")
-else:
-    print("An error occured on email send")
